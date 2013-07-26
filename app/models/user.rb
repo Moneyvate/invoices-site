76e213d87717c88924d9f01b7aaa2dde1dc1b8e0
Nick Smith <1hockeyplayer+github@gmail.com>
2013-07-26 16:42:04 -0700

    Clean Up Devise Views

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :token_authenticatable
+
+  before_save :ensure_authentication_token
+
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
+
+  # Allows an associated mobile app to confirm a valid user immediately
+  def skip_confirmation!
+    self.confirmed_at = Time.now
+  end
+end
