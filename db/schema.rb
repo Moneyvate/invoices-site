8677b540d8aedc4a9088976541d6e6c8eb878c74
Nick Smith <1hockeyplayer+github@gmail.com>
2013-07-31 10:09:25 -0700

    Start Client Management

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130725002225) do
+ActiveRecord::Schema.define(version: 20130730211312) do
+
+  create_table "clients", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username",               default: "", null: false
