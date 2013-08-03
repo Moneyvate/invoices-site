922d2f29997e36d1cd63ca6c22e3902320316d98
Nick Smith <1hockeyplayer+github@gmail.com>
2013-08-02 19:20:46 -0700

    Tweak UI
    * Phone numbers are now correctly parsed on "show" and "index" pages
    * Views are generally more responsive
    * Top bar is now more responsive, and works on mobile

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,4 +5,13 @@ class Contact < ActiveRecord::Base
   validates :contact_type_id, presence: true
   validates :client_id, presence: true
   validates :name, presence: true
+
+  def parse_phone
+    unless phone.nil?
+      format = phone.to_s.split(/^(\d{3})(\d{3})(\d{4})$/)
+      return "(#{format[1]}) #{format[2]}-#{format[3]}"
+    else
+      return "None"
+    end
+  end
 end
