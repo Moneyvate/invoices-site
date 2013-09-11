789be2ee5594f25f1159be83601273f6e49ca46f
Nick Smith <1hockeyplayer+github@gmail.com>
2013-09-11 09:08:22 -0700

    Reformat Model Spec
    Convert tabs to spaces

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe "An instance of", Task do
-	let(:task) { FactoryGirl.build(:task)}
-	
+  let(:task) { FactoryGirl.build(:task) }
+    
   it "should have a name" do
     expect(task.to_s).to eq(task.name)
   end
