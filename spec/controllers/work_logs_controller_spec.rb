8d35e17beb21b385a6e6bed264d74cf74c64bab2
Nick Smith <1hockeyplayer+github@gmail.com>
2013-12-23 15:15:09 -0800

    Correct Redirect Spec

--- spec/controllers/work_logs_controller_spec.rb
+++ spec/controllers/work_logs_controller_spec.rb
@@ -112,7 +112,7 @@ describe WorkLogsController do
       it "redirects to the associated task" do
         work_log = WorkLog.create! valid_attributes
         put :update, {:id => work_log.to_param, :work_log => valid_attributes}, valid_session
-        response.should redirect_to(task_path(work_log))
+        response.should redirect_to(task_path(work_log.task_id))
       end
     end
 
